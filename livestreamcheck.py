b7d0d997ab0d4f4bfbbae20eaac17fb3b42ce47c
NateG <natebgibson@gmail.com>
2022-07-05 22:34:37 -0400

    Config values refactor

--- livestreamcheck.py
+++ livestreamcheck.py
@@ -1,32 +1,42 @@
 #!/home/nate/.local/bin/livestreamcheck/venv/bin/python3
 #regions imports
+import configparser
 import requests
 from pathlib import Path
 #endregion
 
 #region functions
-def check_config(configPath): #Check if the config dir is present, and if not create it and a config file
-    if not (configPath.is_dir()):
-        configPath.mkdir()
-        Path(configPath / "userID").touch()
-        Path(configPath / "clientID").touch()
-        Path(configPath / "token").touch()
-        Path(configPath / "refreshToken").touch()
-        Path(configPath / "clientSecret").touch()
-        print("Please populate the files in '~/.config/livestreamcheck' with their proper values.")
-
-def query_streams(userID, clientID, token):
-    headers = { 'Authorization': 'Bearer ' + token, 'Client-Id': clientID }
-    data = { 'user_id': userID }
+def config_set(configDir, configPath): #Check if the config file is present, and if not create it with dummy values
+    if not (configPath.is_file()):
+        print("Config file not found. Creating dummy file at: " + str(configPath))
+        Path.mkdir(configDir)
+        config = configparser.ConfigParser()
+        Path(configPath).touch()
+        config['TwitchBits'] = {'userID': 'foo', 'clientID': 'bar', 'token': 'fizz', 'refreshToken': 'buzz', 'clientSecret': 'fizzbuzz'}
+        with open(configPath, 'w') as configfile:
+            config.write(configfile)
+        print("Please refer to the README.md to get guidance on how to generate the needed values for the config file.")
+    else:
+        #Populate vars
+        print("Populating varibles...")
+        config = configparser.RawConfigParser()
+        config.read(configPath)
+    return config
+        
+def query_streams(config):
+    headers = { 'Authorization': 'Bearer ' + config['TwitchBits']['token'], 'Client-Id': config['TwitchBits']['clientID'] }
+    data = { 'user_id': config['TwitchBits']['userID'] }
     r = requests.get('https://api.twitch.tv/helix/streams/followed', params=data, headers=headers)
     return r
 
-def refresh_token(refreshToken, clientID, clientSecret, configPath):
+def refresh_token(configPath, config):
     print("Renewing Token...")
     headers = { 'Content-Type': 'application/x-www-form-urlencoded' }
-    data = 'grant_type=refresh_token&refresh_token=' + refreshToken + '&client_id=' + clientID + '&client_secret=' + clientSecret
+    data = 'grant_type=refresh_token&refresh_token=' + config['TwitchBits']['refreshToken'] + '&client_id=' + config['TwitchBits']['clientID'] + '&client_secret=' + config['TwitchBits']['clientSecret']
     r = requests.post('https://id.twitch.tv/oauth2/token', headers=headers, data=data)
-    Path(configPath / "token").write_text(r.json()["access_token"])
+    config.set('TwitchBits', 'access_token', r.json()["access_token"])
+    with open(configPath, 'w') as configfile:
+            config.write(configfile)
 
 def write_results(streams):
     print ("\nCHANNEL " + ' '*13 + "GAME" + ' '*37 + "VIEWERS" + ' '*8 + "\n" + '-'*80)
@@ -34,27 +44,21 @@ def write_results(streams):
          print ("{} {} {}".format(stream['user_name'].ljust(20), stream['game_name'].ljust(40), str(stream['viewer_count']).ljust(8)))
 #endregion
 
-#region varibles
-configPath = Path('~/.config/livestreamcheck').expanduser()
-userID = Path(configPath / "userID").read_text().strip()
-clientID = Path(configPath / "clientID").read_text().strip()
-token = Path(configPath / "token").read_text().strip()
-refreshToken = Path(configPath / "refreshToken").read_text().strip()
-clientSecret = Path(configPath / "clientSecret").read_text().strip()
-#endregion
-
-#region dostuff
-
+#region main
+configDir = Path('~/.config/livestreamcheck').expanduser()
+configFile = Path('~/.config/livestreamcheck/config').expanduser()
 #Verify config dir is present and create one if it is not. TODO: Walk through values needed in README.md
-check_config(configPath)
+config = config_set(configDir, configFile)
+#Deadman's switch
+if config['TwitchBits']['userID'] == "foo":
+    print("Quitting program. Please populate config file.")
+    quit()
 
-#Use data pulled from config files to query Twitch API. Returns JSON and a status regardless of validity.
-streams = query_streams(userID, clientID, token)
-
+streams = query_streams(config)
 if streams.ok:    
     write_results(streams)
 else:
     print("Error getting stream data. Response code: " + str(streams.status_code))
-    refresh_token(refreshToken, clientID, clientSecret, configPath)
+    refresh_token(configFile, config)
     print("Attempting token refresh, please try again")
 #endregion
