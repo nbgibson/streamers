58147199b5c2b9c69ccaa808637839421c0fd1d8
Nate Gibson <natebgibson@gmail.com>
2023-02-26 00:58:17 -0500

    Logs, cleaned up code, started config file rewrite

--- streamers/streamers.py
+++ streamers/streamers.py
@@ -2,34 +2,33 @@
 
 # region honeydo_list
 
-# TODO: Spin out VLC integration into it's own thing; let SL stand alone
-# TODO: Add '?' flag to mirror 'h' flag behavior
+# TODO: Set up config file structure to use player and args data
+# TODO: Set user passed CLI flags to override config file settings
 # TODO: See if there is a way to make config file changes backwards compatible
-# TODO: Make token refresh silent without a verbosity flag, auto rerun if token refresh is successful.
 # TODO: Look into making table display customizable in terms of size (auto sizing based on window size?) or colums sortable via config file
 # TODO: Documentation rework for pypi visibility. Github isn't really the focus now. Look into split documentation?
 
 # endregion
 
-# regions imports
+# region imports
 import os
 import configparser  # Config fun
 import requests  # API fun
 from pathlib import Path
-import shutil  # Streamlink check
+import shutil  # Player install check
+import streamlink  # Extraction of m3u8 URIs for VLC
 import argparse
+import logging
 
 # endregion
 
 # region functions
 
 
-def config_set(
-    configDir, configPath
-):  # Check if the config file is present, and if not create it with dummy values
-    if not (configDir.is_dir()):
+def config_set(configDir, configPath):  
+    if not (configDir.is_dir()): # Check if the config dir is present, and if not create it 
         Path.mkdir(configDir, parents=True, exist_ok=True)
-    if not (configPath.is_file()):
+    if not (configPath.is_file()): # Check if the config file is present, and if not create it with dummy values
         print("Config file not found. Creating dummy file at: " + str(configPath))
         config = configparser.ConfigParser()
         Path(configPath).touch()
@@ -40,17 +39,22 @@ def config_set(
             "refreshToken": "buzz",
             "clientSecret": "fizzbuzz",
         }
-        config["StreamLinkBits"] = {"enabled": "false"}
+        config["PlayerBits"] = {
+            "player": "",
+            "arguments": ""
+        }
         with open(configPath, "w") as configfile:
             config.write(configfile)
         print(
-            "Please refer to the README.md to get guidance on how to generate the needed values for the config file."
+            "Please refer to the documentation to get guidance on how to generate the needed values for the config file."
         )
+    # TODO: Check for player section, add if needed
     else:
         # Populate vars
         config = configparser.RawConfigParser()
         config.read(configPath)
     return config
+
 
 def query_streams(config):
     headers = {
@@ -63,8 +67,9 @@ def query_streams(config):
     )
     return r
 
+
 def refresh_token(configPath, config):
-    print("Renewing Token...")
+    logging.debug("Renewing Token...")
     headers = {"Content-Type": "application/x-www-form-urlencoded"}
     data = (
         "grant_type=refresh_token&refresh_token="
@@ -80,80 +85,70 @@ def refresh_token(configPath, config):
     with open(configPath, "w") as configfile:
         config.write(configfile)
 
+
 def write_results(streams, playerFlag):
-    if playerFlag != None:
-        index = 0
-        print(
-            "\nINDEX   CHANNEL "
-            + " " * 13
-            + "GAME"
-            + " " * 37
-            + "VIEWERS"
-            + " " * 8
-            + "\n"
-            + "-" * 80
-        )
-        for stream in streams.json()["data"]:
+    if len(streams.json()["data"]) > 0:
+        if playerFlag != None:
+            index = 0
             print(
-                "{} {} {} {}".format(
-                    str(index).ljust(7),
-                    stream["user_name"].ljust(20)[:20],
-                    stream["game_name"].ljust(40)[:40],
-                    str(stream["viewer_count"]).ljust(8),
+                "\nINDEX   CHANNEL "
+                + " " * 13
+                + "GAME"
+                + " " * 37
+                + "VIEWERS"
+                + " " * 8
+                + "\n"
+                + "-" * 80
+            )
+            for stream in streams.json()["data"]:
+                print(
+                    "{} {} {} {}".format(
+                        str(index).ljust(7),
+                        stream["user_name"].ljust(20)[:20],
+                        stream["game_name"].ljust(40)[:40],
+                        str(stream["viewer_count"]).ljust(8),
+                    )
                 )
-            )
-            index += 1
-    else:
-        print(
-            "\nCHANNEL "
-            + " " * 13
-            + "GAME"
-            + " " * 37
-            + "VIEWERS"
-            + " " * 8
-            + "\n"
-            + "-" * 80
-        )
-        for stream in streams.json()["data"]:
-            print(
-                "{} {} {}".format(
-                    stream["user_name"].ljust(20)[:20],
-                    stream["game_name"].ljust(40)[:40],
-                    str(stream["viewer_count"]).ljust(8),
-                )
-            )
-
-def stream_link(streams, locate):
-    while True:
-        try:
-            maxSel = len(streams.json()["data"])
-            index = -1
-            while index not in range(0, maxSel):
-                index = int(input("Enter index of stream to watch: "))
-        except ValueError:
-            print(
-                "Sorry, I didn't understand that. Enter an integer from 0 to "
-                + str(maxSel - 1)
-            )
-            continue
-        except KeyboardInterrupt:
-            quit()
+                index += 1
         else:
-            break
+            print(
+                "\nCHANNEL "
+                + " " * 13
+                + "GAME"
+                + " " * 37
+                + "VIEWERS"
+                + " " * 8
+                + "\n"
+                + "-" * 80
+            )
+            for stream in streams.json()["data"]:
+                print(
+                    "{} {} {}".format(
+                        stream["user_name"].ljust(20)[:20],
+                        stream["game_name"].ljust(40)[:40],
+                        str(stream["viewer_count"]).ljust(8),
+                    )
+                )
+    else:
+        print("No followed streams online at this time.")
 
-    streamer = streams.json()["data"][index]["user_name"]
-    os.system(locate + " https://twitch.tv/" + streamer)
 
 def config_args():
     parser = argparse.ArgumentParser(
-        prog = 'Streamers',
+        prog='Streamers',
         description="Get a list of followed Twitch live streams from the comfort of your own CLI and optionall stream them."
+    )
+    parser.add_argument(
+        '-l',
+        '--logging',
+        help="Adds additional output/verbosity for troublshooting.",
+        action="store_true"
     )
     parser.add_argument(
         "-p",
         "--player",
         required=False,
-        choices=['streamlink','iina','mpv'],
+        choices=['iina', 'mpv', 'streamlink', 'vlc'],
         help="Pass in your preferred player if desired. Available options: Streamlink, IINA, and MPV. Presumes you have the passed player installed and configured to take inputs via CLI.",
     )
     parser.add_argument(
@@ -165,9 +160,10 @@ def config_args():
         default='',
         help="Optionally pass arguments to be used with your player. HINT: Use the format: -a=\"--optional-arguments\" to pass in content with dashes so as to not conflict with argparse's parsing."
     )
-    
+
     args = parser.parse_args()
     return args
+
 
 def player_output(player, streams, args):
     while True:
@@ -189,29 +185,35 @@ def player_output(player, streams, args):
     stream = streams.json()["data"][index]["user_name"]
     start_player(stream, args)
 
+
 def start_player(stream, args):
-    #try catch to verify the selected player is installed/PATH accessable
     playerPath = shutil.which(args.player)
     if playerPath != None:
-        #Start stream
-        print("Starting stream")
+        # Start stream
+        print("----------Starting stream----------")
         if args.player == "mpv" or args.player == "streamlink" or args.player == "iina":
-            print("Starting " + args.player + " with command: " + playerPath + " " + args.arguments + " https://twitch.tv/" + stream)
-            os.system(playerPath + " " + args.arguments + " https://twitch.tv/" + stream)
+            logging.debug("Starting " + args.player + " with command: " + playerPath +
+                  " " + args.arguments + " https://twitch.tv/" + stream)
+            os.system(playerPath + " " + args.arguments +
+                      " https://twitch.tv/" + stream)
         elif args.player == "vlc":
-            print("foo")
+            streams = streamlink.streams("https://twitch.tv/" + stream)
+            logging.debug("Starting " + args.player + " with command: " +
+                  playerPath + " " + args.arguments + " " + streams["best"].url)
+            os.system(playerPath + " --meta-title \"" + stream + "\" --video-title \"" +
+                      stream + "\" " + args.arguments + " " + streams["best"].url)
 
     else:
         print(args.player + " is either not installed or on the system's PATH. Please verify that it is present and retry.")
-    
-    #os.system(playerPath + " https://twitch.tv/" + streamer)
-    #print("Player: :" + args.player + "\n playerPath + " + playerPath)
 
 # endregion
 
 # region main
+
 def main():
     args = config_args()
+    if args.logging:
+        logging.basicConfig(format='DEBUG: %(message)s',level=logging.DEBUG)
     # region config
     configDir = Path("~/.config/streamers").expanduser()
     configFile = Path("~/.config/streamers/config").expanduser()
@@ -221,52 +223,17 @@ def main():
         print("Quitting program. Please populate config file.")
         quit()
     streams = query_streams(config)
-    
+
+    if not streams.ok:
+        logging.debug("Attempting token refresh.")
+        refresh_token(configFile, config)
+
     if streams.ok:
-        if len(streams.json()["data"]) > 0:
-            write_results(streams, args.player)
-            if args.player != None:
-                player_output(args.player, streams, args)
-        else:
-            print("No followed streams online at this time.")
+        write_results(streams, args.player)
+        if args.player != None:
+            player_output(args.player, streams, args)
     else:
         print("Error getting stream data. Response code: " +
               str(streams.status_code))
-        refresh_token(configFile, config)
-        print("Attempting token refresh, please try again")
-    
-
-
-#streamlink --json twitch.tv/unlimitedsteam | jq -r '.streams.best.url'
-#vlc --meta-title "butts" --video-title "butts" https://video-weaver.atl01.hls.ttvnw.net/v1/playlist/Cq0EgR_ErttiWkmiXFt6s7x_dRji9NBVDzN2s97SZIPxBCmh7JG5aAR-Fb4Hy4ZVUHs5ZdbCCkIBPKJO44GFanlD56O3W8sCZBeNgEmcG2sHd8t2WPOEiVlmp9J9OR60Mqd3vhNeVPhTgzzWzu12_QHtpaqmTHgV6l_v-ZBRfW8r0MZjbGYdHooLW2Puhu1CCYvBcEydAs8rREJKVIDLPj4169ovaNDICE7yRQ_anYpTyuOBmlBwMb-49n4XXZgGrdOHco4vmFblYJaB_fqVzpErd8jM1bJHg3iqNkeGuc0DGFLfW6K1Xm6NA3QMMUHgON_QLO-XoDIfuDf2IB-dkR8OaJErRmMWbRITqw64y2o7rB4WefoYcuQLngE1duUhVUsl8p0Qa0WLFs01tjThZohyISsNAOZaaesx-hANQnZQXlCBYbg7BVbQjOQTB0OMTHez79EbHCptxIruAklnvNjVdON6uPaT5nCcwn5sGhABWFyQtzih0AVzzSDVY5v5bzYS-EvuHr1iMUvWSfJKgjMNoqsX3yPzQeLHxtcwCgKvScvLwAGgQ1JgIUhcL3YMtP1C7ttQRSeQwYvsDxZnesI5xZQof8DHcjh_dFpiT8pOTKDZjix_xQoFlyhq0hRhsng0hgpPpf260EYbU1g-LSh2862JQa4lG7RkGqTgXWdGphWGGX9h2AWkC-aq5ReCQgMinaMmswleNYI8W_7sYV68AIfXx_wmvzFxrSr5Bv8aDEHgLltMofkYVMANACABKgl1cy13ZXN0LTIw-AU.m3u 
-    
-    
-    
-#    try:
-#        if args.player == 'streamlink' or config["StreamLinkBits"]["enabled"].lower() == "true":
-#            streamLinkFlag = True
-#            print("Streamlink enabled")
-#        else:
-#            streamLinkFlag = False
-#    except KeyError:
-#        print("")
-#        print(
-#            "Missing ['StreamLinkBits'] section of the config file. Please refer to the documentation for an example config containing it."
-#        )
-#        quit()
-#    if streams.ok:
-#        if len(streams.json()["data"]) > 0:
-#            write_results(streams, streamLinkFlag)
-#            locate = shutil.which("streamlink")
-#            if locate and streamLinkFlag:
-#                print("")
-#                stream_link(streams, locate)
-#        else:
-#            print("No followed streams online at this time.")
-#    else:
-#        print("Error getting stream data. Response code: " +
-#              str(streams.status_code))
-#        refresh_token(configFile, config)
-#        print("Attempting token refresh, please try again")
 
 # endregion
